a678e2270514ad6fa58106ff3052343e2807bbfd
mudassarali.official <mudassarali.official@gmail.com>
2024-03-07 06:55:07 +0500

    11 Shop Module Added in Application.

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ShopComponent } from './shop/shop.component';
+import { ShopComponent } from './shop.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from '../shared/shared.module';
 import { CoreModule } from '../core/core.module';
-import { ProductItemComponent } from './shop/product-item/product-item.component';
-import { AppRoutingModule } from '../app-routing.module';
-
-
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ShopRoutingModule } from './shop-routing.module';
 @NgModule({
   declarations: [
     ShopComponent,
-    ProductItemComponent
+    ProductItemComponent,
+    ProductDetailsComponent,
   ],
   imports: [
-    AppRoutingModule,
     CommonModule,
     HttpClientModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    ShopRoutingModule,
+  ],
+  exports: [
+    ShopComponent
   ]
 })
 export class ShopModule { }
