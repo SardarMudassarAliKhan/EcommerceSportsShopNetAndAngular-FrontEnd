fc9199df3861d678b07cc6e4f21bdea17e484b9b
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2024-03-20 06:25:16 +0500

    44-Stripe Payment Gateway Integration with website

--- src/app/checkout/checkout.module.ts
+++ src/app/checkout/checkout.module.ts
@@ -8,6 +8,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CheckoutDeliveryComponent } from './checkout-delivery/checkout-delivery.component';
 import { CheckoutReviewComponent } from './checkout-review/checkout-review.component';
 import { CheckoutSuccessComponent } from './checkout-success/checkout-success.component';
+import { CheckoutPaymentComponent } from './checkout-payment/checkout-payment.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { CheckoutSuccessComponent } from './checkout-success/checkout-success.co
     CheckoutAddressComponent,
     CheckoutDeliveryComponent,
     CheckoutReviewComponent,
-    CheckoutSuccessComponent
+    CheckoutSuccessComponent,
+    CheckoutPaymentComponent
   ],
   imports: [
     CommonModule,
